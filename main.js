72b4d4ba29c935ea1799293d3d55f4ee22f48c1f
Eldohub Academy <academy@eldohub.co.ke>
2025-03-24 11:00:49 +0300

    results done

--- main.js
+++ main.js
@@ -7,24 +7,62 @@ ourForm.addEventListener("submit", (event) => {
   const termInput = document.getElementById("term");
   const repaymentInput = document.getElementById("repayment");
   const interestOnlyInput = document.getElementById("interest-only");
-
-  console.log(amountInput.value);
-  console.log(rateInput.value);
-  console.log(termInput.value);
-  console.log(repaymentInput.checked);
-  console.log(interestOnlyInput.checked);
+  let errorCount = 0;
 
   if (repaymentInput.checked || interestOnlyInput.checked) {
     document.getElementById("radio-error").style.display = "none";
   } else {
     document.getElementById("radio-error").style.display = "block";
+    errorCount++;
   }
-  // an empty string is a falsy value -- evalues to fales if put as a condition
-  console.log(Number(amountInput.value));
-
   if (Number(amountInput.value) && Number(amountInput.value) > 0) {
     document.getElementById("amount-error").style.display = "none";
   } else {
     document.getElementById("amount-error").style.display = "block";
+    errorCount++;
+  }
+  if (Number(rateInput.value) && Number(rateInput.value) > 0) {
+    document.getElementById("rate-error").style.display = "none";
+  } else {
+    document.getElementById("rate-error").style.display = "block";
+    errorCount++;
+  }
+  if (Number(termInput.value) && Number(termInput.value) > 0) {
+    document.getElementById("term-error").style.display = "none";
+  } else {
+    document.getElementById("term-error").style.display = "block";
+    errorCount++;
+  }
+  if (errorCount == 0) {
+    showResult(
+      Number(amountInput.value),
+      Number(rateInput.value),
+      Number(termInput.value),
+      repaymentInput.checked,
+      interestOnlyInput.checked
+    );
   }
 });
+function showResult(amount, rate, term, isRepayment, isInterestOnly) {
+  document.querySelector(".empty-result").style.display = "none";
+  document.querySelector(".completed-result").style.display = "block";
+  // calculate mortgage and show results
+  if (isInterestOnly) {
+    // chosen the radio for interest only
+    let totalInterest = amount * (rate / 100) * term;
+    document.querySelector(".results-container").innerHTML = `
+      <h3>Your Total interest</h3>
+      <p> ${totalInterest.toLocaleString()} </p>
+      
+    `;
+  }
+  if (isRepayment) {
+    // chosen repayment -- UI
+    let totalRepayment = amount + amount * (rate / 100) * term;
+    let monthlyRepayment = totalRepayment / (term * 12);
+    document.querySelector("#monthly-repayment-result").textContent =
+      monthlyRepayment.toLocaleString();
+    document.querySelector("#total-repayment-result").textContent =
+      totalRepayment.toLocaleString();
+  }
+}
